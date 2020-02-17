4f245aec4928fbd5217a4cc84db603c251c6f9fd
Josh.hong <hong@mz.co.kr>
2020-02-17 16:45:43 +0900

    .

--- article-service/src/main/kotlin/handson/article/ArticleService.kt
+++ article-service/src/main/kotlin/handson/article/ArticleService.kt
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.patchForObject
-import java.net.URI
 import java.util.*
 import javax.persistence.*
 
